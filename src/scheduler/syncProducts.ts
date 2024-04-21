023081409a6900d3a308fb0b0b21a8e1225378cd
Mario Novak <marionovak5@gmail.com>
2024-04-21 06:36:05 +0200

    scheduler: sync products

--- src/scheduler/syncProducts.ts
+++ src/scheduler/syncProducts.ts
@@ -1,1 +1,28 @@
 console.log("Hello, Heroku Scheduler!");
+
+import app from "../app";
+import server from "../server";
+import { databaseSetup } from "../database";
+import luceedProductInventoryService from "../modules/luceed/services/luceedProductInventory.service";
+import luceedService from "../modules/luceed/services/luceed.service";
+
+// if (process.env.NODE_ENV !== "test") {
+// }
+
+async function syncLuceedProductsToShopify() {
+  await databaseSetup();
+  console.log("Schelduer: Sync: inside call begin");
+  const luceedProducts =
+    await luceedProductInventoryService.fetchProductsWithInventory();
+  const response = await luceedService.syncLuceedShopifyProducts(
+    luceedProducts
+  );
+  console.log("Schelduer: Sync: inside call end", response);
+  return response;
+}
+
+console.log("Schelduer: Sync: before call");
+syncLuceedProductsToShopify();
+console.log("Schelduer: Sync: after call");
+
+export { app, server };
